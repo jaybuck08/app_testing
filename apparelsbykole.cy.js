2df8721a56233904229134bace95faccfaace1b1
jaybuck08 <150525828+jaybuck08@users.noreply.github.com>
2024-02-26 21:40:43 +0000

    Update apparelsbykole.cy.js

--- apparelsbykole.cy.js
+++ apparelsbykole.cy.js
@@ -4,12 +4,12 @@ describe('Shopping', () => {
     it('Buy Gown', () => {
         cy.visit("https://apparelsbykole.com/")
         cy.get('.post-894 > .grve-isotope-item-inner > .grve-hover-item > .grve-media > .grve-item-url').click()
-        // cy.get('.grve-plus').click().click()    ------ this clicks two times
+        
         cy.get('.grve-plus').click()
         cy.get('.single_add_to_cart_button').click()
         cy.get('#grve-main-responsive-header > .grve-header-row > .grve-right-area > .grve-header-elements > .grve-header-element > a > .grve-purchased-items').click()
         cy.get('.wc-block-components-button__text').click()
-        //cy.get('a.wc-block-components-button').click()
+   
 
         //shipping information
         cy.get('#email').type("felix.njoku@outlook.com");
